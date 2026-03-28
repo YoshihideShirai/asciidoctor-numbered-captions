0fc94fc0e2c5d6d7340c2cffb72c46644a6c62c2
Yoshihide Shirai <yoshihide.shirai@gmail.com>
2026-03-28 10:32:32 +0900

    Initialize Asciidoctor caption numbering plugin

---
+++ src/index.js
@@ -0,0 +1,98 @@
+'use strict'
+
+const DEFAULT_LABELS = {
+  image: 'Figure',
+  table: 'Table',
+  stem: 'Equation'
+}
+
+function register (registry, options = {}) {
+  const chapterLevel = Number.isInteger(options.chapterLevel) ? options.chapterLevel : 1
+  const labels = { ...DEFAULT_LABELS, ...(options.labels || {}) }
+
+  registry.postprocessor(function () {
+    this.process(function (_document, output) {
+      const lines = output.split('\n')
+
+      let chapter = 0
+      const counters = {
+        image: 0,
+        table: 0,
+        stem: 0
+      }
+      let blockContext = null
+
+      const resetCounters = () => {
+        counters.image = 0
+        counters.table = 0
+        counters.stem = 0
+      }
+
+      for (let i = 0; i < lines.length; i += 1) {
+        const line = lines[i]
+
+        if (chapterLevel === 1 && line.includes('<div class="sect1">')) {
+          chapter += 1
+          resetCounters()
+          blockContext = null
+          continue
+        }
+
+        if (line.includes('<div class="imageblock">')) {
+          blockContext = 'image'
+          continue
+        }
+
+        if (line.includes('<table class="tableblock')) {
+          blockContext = 'table'
+          continue
+        }
+
+        if (line.includes('<div class="stemblock">')) {
+          blockContext = 'stem'
+          continue
+        }
+
+        if (blockContext === 'image' && line.includes('<div class="title">')) {
+          counters.image += 1
+          const effectiveChapter = chapter || 1
+          lines[i] = line.replace(
+            /<div class="title">.*?\.\s*/,
+            `<div class="title">${labels.image} ${effectiveChapter}-${counters.image}. `
+          )
+          blockContext = null
+          continue
+        }
+
+        if (blockContext === 'table' && line.includes('<caption class="title">')) {
+          counters.table += 1
+          const effectiveChapter = chapter || 1
+          lines[i] = line.replace(
+            /<caption class="title">.*?\.\s*/,
+            `<caption class="title">${labels.table} ${effectiveChapter}-${counters.table}. `
+          )
+          blockContext = null
+          continue
+        }
+
+        if (blockContext === 'stem' && line.includes('<div class="title">')) {
+          counters.stem += 1
+          const effectiveChapter = chapter || 1
+          lines[i] = line.replace(
+            /<div class="title">/,
+            `<div class="title">${labels.stem} ${effectiveChapter}-${counters.stem}. `
+          )
+          blockContext = null
+          continue
+        }
+      }
+
+      return lines.join('\n')
+    })
+  })
+}
+
+module.exports = {
+  register,
+  DEFAULT_LABELS
+}
---
+++ test/plugin.test.js
@@ -0,0 +1,91 @@
+'use strict'
+
+const test = require('node:test')
+const assert = require('node:assert/strict')
+const asciidoctor = require('asciidoctor')()
+const { register } = require('../src/index')
+
+function convertWithPlugin (source, options = {}) {
+  const registry = asciidoctor.Extensions.create()
+  register(registry, options)
+
+  return asciidoctor.convert(source, {
+    safe: 'safe',
+    extension_registry: registry,
+    attributes: {
+      stem: 'latexmath',
+      sectnums: '',
+      ...options.attributes
+    }
+  })
+}
+
+test('numbers image/table/equation captions as chapter-counter', () => {
+  const input = `= Sample
+
+== Chapter One
+
+.Figure One
+image::one.png[]
+
+.Table One
+|===
+|A |B
+|===
+
+.Einstein
+[stem]
+++++
+E = mc^2
+++++
+
+== Chapter Two
+
+.Figure Two
+image::two.png[]
+
+.Table Two
+|===
+|C |D
+|===
+
+.Newton
+[stem]
+++++
+F = ma
+++++
+`
+
+  const html = convertWithPlugin(input)
+
+  assert.match(html, /Figure 1-1\./)
+  assert.match(html, /Table 1-1\./)
+  assert.match(html, /Equation 1-1\./)
+  assert.match(html, /Figure 2-1\./)
+  assert.match(html, /Table 2-1\./)
+  assert.match(html, /Equation 2-1\./)
+})
+
+test('resets counters per chapter and per block type', () => {
+  const input = `= Sample
+
+== Chapter One
+
+.First Figure
+image::one.png[]
+
+.Second Figure
+image::two.png[]
+
+== Chapter Two
+
+.Third Figure
+image::three.png[]
+`
+
+  const html = convertWithPlugin(input)
+
+  assert.match(html, /Figure 1-1\./)
+  assert.match(html, /Figure 1-2\./)
+  assert.match(html, /Figure 2-1\./)
+})
